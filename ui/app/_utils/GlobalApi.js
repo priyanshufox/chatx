874e5dbcd062c9849ddb9a0de391a8549eb3d988
Priyanshu <priyanshugevra@gmail.com>
2024-04-24 15:29:22 +0530

    added code for accessing envirnment variable

--- ui/app/_utils/GlobalApi.js
+++ ui/app/_utils/GlobalApi.js
@@ -4,28 +4,28 @@ import { data } from "autoprefixer";
 const { default: axios } = require("axios");
 
 //created axio client to create endpoint
-const axiosClient=axios.create({
-    baseURL:'http://localhost:8000'
+const axiosClient = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_SERVER_HOST
 });
 
-const createUser = (data) => axiosClient.post('/user',data);
-const getUserByEmail = (email) => axiosClient.get('/user/'+email);
-const createPost = (data) => axiosClient.post('/post',data);
+const createUser = (data) => axiosClient.post('/user', data);
+const getUserByEmail = (email) => axiosClient.get('/user/' + email);
+const createPost = (data) => axiosClient.post('/post', data);
 const getAllPost = () => axiosClient.get('/post');
-const onPostLike =(postId,data) => axiosClient.put('/post/like/'+postId,data);
+const onPostLike = (postId, data) => axiosClient.put('/post/like/' + postId, data);
 // add new comment
-const addComment = (data) => axiosClient.post('/comment',data);
+const addComment = (data) => axiosClient.post('/comment', data);
 //delete comment
-const deleteComment = (commentId) => axiosClient.delete('/comment/'+commentId);
+const deleteComment = (commentId) => axiosClient.delete('/comment/' + commentId);
 
 
-export default{
+export default {
     createUser,
     getUserByEmail,
     createPost,
     getAllPost,
     onPostLike
-    ,addComment,
+    , addComment,
     deleteComment
 
 }
